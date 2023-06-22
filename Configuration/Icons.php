1255ed8fe717084501c4724a1bcf5916f197d41b
nanna-dk <nanna@e-nanna.dk>
2023-06-22 13:51:24 +0200

    Setup

---
+++ Configuration/Icons.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Icon Registry
+ */
+
+defined('TYPO3') || die();
+
+return [
+    // icon identifier
+    'ucph_layout_one_column_icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:ucph_layout_one_column/Resources/Public/Icons/Extension.svg',
+    ],
+];
---
+++ README.md
@@ -0,0 +1,4 @@
+TYPO3 extension content element "regular text"
+==============================================================
+
+TYPO3 v. 11.5, PHP 8.1.
